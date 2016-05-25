f78bc342d60e708118fcc3c57114c3b5e8073177
Paulie <wqy630@qq.com>
2016-05-25 23:24:10 +0800

    Add web source code and README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# SafeMove
+* SafeMove 反映不同地区历史安全数据的服务
+* Django
+* 可以访问WWW.pauliewu.cn查看Web当前样式
+* 高德API
---
+++ SaveMoveWeb/Amap.js
@@ -0,0 +1,6 @@
+  var map = new AMap.Map('AmapContainer', {
+        resizeEnable: true,
+        zoom:11,
+        center: [116.397428, 39.90923]
+        
+    });
