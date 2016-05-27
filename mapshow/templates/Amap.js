6e4780ba4058f77f2533e9dbb4e1e0d405b0e78d
Paulie <wqy630@qq.com>
2016-05-27 22:34:44 +0800

    add upload.html and a multilayer menu

--- mapshow/templates/Amap.js
+++ mapshow/templates/Amap.js
@@ -1,6 +1,31 @@
-  var map = new AMap.Map('AmapContainer', {
-        resizeEnable: true,
-        zoom:11,
-        center: [116.397428, 39.90923]
-        
+// amap
+var map = new AMap.Map('AmapContainer', {
+        resizeEnable: true,        
     });
+        
+var buttons = document.querySelectorAll(".radmenu a");
+
+// circle menu
+for (var i=0, l=buttons.length; i<l; i++) {
+  var button = buttons[i];
+  button.onclick = setSelected;
+}
+
+function setSelected(e) {
+    if (this.classList.contains("selected")) {
+      this.classList.remove("selected");
+      if (!this.parentNode.classList.contains("radmenu")) {
+        this.parentNode.parentNode.parentNode.querySelector("a").classList.add("selected")
+      } else {
+        this.classList.add("show");
+      }
+    } else {
+      this.classList.add("selected");
+      if (!this.parentNode.classList.contains("radmenu")) {
+        this.parentNode.parentNode.parentNode.querySelector("a").classList.remove("selected")
+      } else {
+        this.classList.remove("show");
+      }
+    }
+    return false;
+}
